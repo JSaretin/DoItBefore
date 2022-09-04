ce0399bac39da4a7c03b4ea2e490303d662a6b02
v0idhack <johnosaretin7878@gmail.com>
2022-09-04 23:37:08 +0100

    add fallback and using env

--- src/routes/view-[countdown_id]/+page.server.ts
+++ src/routes/view-[countdown_id]/+page.server.ts
@@ -1,8 +1,12 @@
 import { base } from '$lib/deta';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad, Action } from '../$types';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const countdown = await base.get(params.countdown_id);
+    if (!countdown) {
+        throw error(404, 'countdown not found')
+    }
 	return countdown;
 };
 
