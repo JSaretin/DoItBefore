526f961fada4739f1ac821043896aa60930794d6
v0idhack <johnosaretin7878@gmail.com>
2022-09-06 14:23:38 +0100

    sort countdown based on time created

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,21 +2,21 @@
 	import CountDown from '$lib/componets/CountDown.svelte';
 	import CreateCountdown from '$lib/componets/CreateCountdown.svelte';
 
-	import type { SavedData } from '$lib/structure';
+	import type { SavedCountdown } from '$lib/structure';
 	import { writable, type Writable } from 'svelte/store';
 
-	export let data: { countdowns: SavedData[]; user: { key?: string } };
-	const countdowns: Writable<SavedData[]> = writable(data.countdowns);
+	export let data: { countdowns: SavedCountdown[]; user: { key?: string } };
+	const countdowns: Writable<SavedCountdown[]> = writable(data.countdowns);
 
-	const updateCountdowns = (data: { details: SavedData }) => {
-		$countdowns = [data.details, ...$countdowns];
+	const updateCountdowns = (countdown: { detail: SavedCountdown }) => {
+		$countdowns = [countdown.detail, ...$countdowns];
 	};
 </script>
 
 <div class="container">
 	<CreateCountdown on:addcountdown={updateCountdowns} user={data.user} />
 	<div class="countdowns">
-		{#each $countdowns as countdown (countdown.key)}
+		{#each $countdowns.sort((a, b) => b.created_at - a.created_at) as countdown (countdown.key)}
 			<CountDown {countdown} />
 		{/each}
 	</div>
