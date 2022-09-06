9770a2aa821f953f90e361895f20eb3f62845910
v0idhack <johnosaretin7878@gmail.com>
2022-09-06 14:24:33 +0100

    added backend restriction

--- src/routes/view-[countdown_id]/+page.server.ts
+++ src/routes/view-[countdown_id]/+page.server.ts
@@ -1,4 +1,5 @@
 import { deta } from '$lib/deta';
+import type { SavedCountdown } from '$lib/structure';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad, Action } from '../$types';
 
@@ -7,10 +8,45 @@ export const load: PageServerLoad = async ({ params }) => {
 	if (!countdown) {
 		throw error(404, 'countdown not found');
 	}
-	return countdown;
+	return {
+		countdown
+	};
 };
 
-export const POST: Action = async ({ request, params }) => {
-	const data = await request.json();
-	await deta.Base('doitbefore').update({ todos: data }, params.countdown_id);
+export const POST: Action = async ({ request, params, locals }) => {
+	const countdown: SavedCountdown = await request.json();
+	if (countdown.key != params.countdown_id) {
+		return {
+			status: 400,
+			errors: {
+				message: 'bad request'
+			}
+		};
+	}
+
+	const savedCountdown: SavedCountdown | null = await deta
+		.Base('doitbefore')
+		.get(params.countdown_id);
+
+	if (!savedCountdown) {
+		return {
+			status: 400,
+			errors: {
+				message: 'not found'
+			}
+		};
+	}
+
+	const canEdit =
+		locals?.user?.key == savedCountdown.owner ||
+		savedCountdown.can_edit.filter((obj) => obj.user_id == locals?.user?.key).length != 0;
+	if (!canEdit) {
+		return {
+			status: 401,
+			errors: {
+				message: 'not countdown owner'
+			}
+		};
+	}
+	await deta.Base('doitbefore').update({ todos: countdown.todos }, countdown.key);
 };
