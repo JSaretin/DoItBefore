8e9ca317c9322e815a8eda52e09d72af024a1c64
v0idhack <johnosaretin7878@gmail.com>
2022-09-06 14:22:29 +0100

    added more data structure

--- src/lib/structure.ts
+++ src/lib/structure.ts
@@ -1,10 +1,31 @@
-export interface Data {
+type Permission = 'delete' | 'update' | 'edit' | 'privacy';
+
+export interface User {
+	key: string;
+}
+
+export interface FullUser extends User {
+	email: string;
+	name: string;
+}
+
+export interface CountdownForm {
 	description: string;
 	todos: { title: string; id: number; completed: boolean }[];
 	deadline: string;
-	// lastupdated?: number;
+	is_public: boolean;
+}
+export interface CountdownToSave extends CountdownForm {
+	owner: string;
+	can_edit: { user_id: User['key']; perm: Permission[] }[];
+	created_at: number;
 }
 
-export interface SavedData extends Data {
+export interface SavedCountdown extends CountdownToSave {
 	key: string;
+}
+
+export interface PageConfig {
+	showInfo: boolean;
+	showTodos: boolean;
 }
